539c43eca80f042d4d4cfa353219837539f0d7ce
Theo186186 <Guietcibois@gmail.com>
2026-08-07 22:31:01 +0200

    Update msgAnnonce.js

--- commands/msgAnnonce.js
+++ commands/msgAnnonce.js
@@ -1,33 +1,37 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
+const { setPending } = require('../utils/pendingPanels');
 
 module.exports = {
   guildScope: 'main',
   data: new SlashCommandBuilder()
     .setName('msg')
     .setDescription('Envoie un message panel (embed) dans ce salon')
-    .addStringOption((opt) => opt.setName('message').setDescription('Texte du message (utilise \\n pour les retours à la ligne)').setRequired(true))
     .addStringOption((opt) => opt.setName('couleur').setDescription('Code couleur hex, ex: #5865F2').setRequired(true))
     .addStringOption((opt) => opt.setName('titre').setDescription('Titre du panel (optionnel)').setRequired(false))
     .addStringOption((opt) => opt.setName('banniere').setDescription('URL d\'une image affichée en bas (optionnel)').setRequired(false)),
 
   async execute(interaction) {
-    const rawMessage = interaction.options.getString('message');
     const color = interaction.options.getString('couleur');
     const titre = interaction.options.getString('titre');
     const banniere = interaction.options.getString('banniere');
-
-    const description = rawMessage.replace(/\\n/g, '\n');
 
     if (!/^#[0-9A-Fa-f]{6}$/.test(color)) {
       return interaction.reply({ content: '❌ Couleur invalide. Utilise un format hex, ex: #5865F2.', ephemeral: true });
     }
 
-    const embed = new EmbedBuilder().setDescription(description).setColor(color);
-    if (titre) embed.setTitle(titre);
-    if (banniere) embed.setImage(banniere);
+    setPending(interaction.user.id, { color, titre, banniere, channelId: interaction.channel.id });
 
-    await interaction.channel.send({ embeds: [embed] })
-      .catch((err) => console.error('[Msg Annonce] Erreur:', err.message));
-    await interaction.reply({ content: '✅ Message envoyé.', ephemeral: true });
+    const modal = new ModalBuilder().setCustomId('panel_modal').setTitle('Contenu du message');
+
+    const textInput = new TextInputBuilder()
+      .setCustomId('panel_message')
+      .setLabel('Texte du message')
+      .setStyle(TextInputStyle.Paragraph)
+      .setRequired(true)
+      .setMaxLength(4000);
+
+    modal.addComponents(new ActionRowBuilder().addComponents(textInput));
+
+    await interaction.showModal(modal);
   }
 };
